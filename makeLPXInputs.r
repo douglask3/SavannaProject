15ecb405edfbc7d5590698281ad148dfce87891f
Douglas Kelley <douglas.kelley@mq.edu.au>
2015-03-13 15:51:26 +1100

    oeing up detrended and adding to front annd addin on reptation at end

--- makeLPXInputs.r
+++ makeLPXInputs.r
@@ -13,16 +13,28 @@ repeatN <- 3
 ###############################
 ## regrid new data           ##
 ###############################
-In =  brick(fileIn[1])[[1:120]]
-tempFileNo=0
+In  = brick(fileIn[1])[[1:120]]
+old = brick(fileIn[2])
+samp= old[[1]]
+layerNo=0
 regridWind <- function(r) {
-    tempFileNo <<- tempFileNo + 1
-    print( tempFileNo )
+    layerNo <<- layerNo + 1
+    print( layerNo )
     tempFileName=paste("temp/r",tempFileNo,".nc",sep="")
     r=disaggregate(r,5)
+    r=resample(r,samp)
     return(writeRaster(r,filename=tempFileName,overwrite=TRUE))
 }
 
 
 regrid=layer.apply(In,regridWind)
 
+rept  = regrid[[(layerNo-11):layerNo]]
+
+wind=addLayer(old,regrid)
+for (i in 1:repeatN) wind=addLayer(wind,rept)
+
+writeRaster(wind,fileOut)
+
+
+    
