84371a981fa93427ede41d48a4acd94477e12dcb
Douglas Kelley <douglas.kelley@mq.edu.au>
2015-03-16 13:41:26 +1100

    fiixng extent problem and cropping to specific area

--- cfg.r
+++ cfg.r
@@ -2,3 +2,5 @@ library("gitProjectExtras")
 setupProjectStructure()
 sourceAllLibs()
 library("raster")
+
+extentDefault = extent(c(113,154,-40,-10))
--- makeLPXInputs.r
+++ makeLPXInputs.r
@@ -3,38 +3,42 @@ source("cfg.r")
 ##Make wind
 
 fileIn  <- c('data/wspd.sig995.mon.mean.nc',
-             'data/uvas_19512000detr.nc')
+             'data/uvas_19512000detr_new.nc')
 
 fileOut <- 'outputs/uvas_wspd.sig995.mon.mean.1900-2013.nc'
-
 repeatN <- 3
+layerNo <- 0
 
+oldChop <- 2
+
+regridWind <- function(r) {
+    r=disaggregate.crop(r,extent=extentDefault,fact=5)
+    r=resample(r,samp)
+}
+
+FUN.memSafe <- function(r,FUN,...) {
+    layerNo <<- layerNo + 1
+    print( layerNo )
+    tempFileName=paste("temp/r",layerNo,sep="")
+    r=FUN(r,...)
+    return(writeRaster(r,filename=tempFileName,overwrite=TRUE))
+}
 
 ###############################
 ## regrid new data           ##
 ###############################
-In  = brick(fileIn[1])[[1:120]]
+In  = brick(fileIn[1])
 old = brick(fileIn[2])
-samp= old[[1]]
-layerNo=0
-regridWind <- function(r) {
-    layerNo <<- layerNo + 1
-    print( layerNo )
-    tempFileName=paste("temp/r",tempFileNo,".nc",sep="")
-    r=disaggregate(r,5)
-    r=resample(r,samp)
-    return(writeRaster(r,filename=tempFileName,overwrite=TRUE))
-}
 
+old = layer.apply(old,crop,y=extentDefault)
 
-regrid=layer.apply(In,regridWind)
-
+samp = old[[1]]
+regrid=layer.apply(In,FUN.memSafe,regridWind)
 rept  = regrid[[(layerNo-11):layerNo]]
 
-wind=addLayer(old,regrid)
+wind=addLayer(old,old[[1:(nlayers(old)-12*oldChop)]],regrid)
 for (i in 1:repeatN) wind=addLayer(wind,rept)
 
-writeRaster(wind,fileOut)
+writeRaster(wind,fileOut,overwrite=TRUE)
 
 
-    
