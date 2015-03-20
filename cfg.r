6db978ebe26622cb271d29f9f49ac1d864f85c53
Douglas Kelley <douglas.kelley@mq.edu.au>
2015-03-20 13:13:18 +1100

    setup for shorter test run

--- cfg.r
+++ cfg.r
@@ -5,4 +5,12 @@ library("raster")
 library("ncdf")
 
 extentDefault   = extent(c(113,154,-40,-10))
-detrendedLength = 600
+
+testRun         = TRUE
+
+detrendedLength = c(full=600,
+                    test=12)
+                    
+                    
+if (testRun) detrendedLength=detrendedLength['test']
+    else detrendedLength=detrendedLength['full']
--- libs/regridData.r
+++ libs/regridData.r
@@ -2,11 +2,15 @@ regridData <- function(fileTS3.1,fileDetr,FUN,...) {
     ###############################
     ## regrid new data           ##
     ###############################
-    In  = brick(fileTS3.1)
-    old = brick(fileDetr)
-
-    old = layer.apply(old,FUN.memSafe,crop,y=extentDefault)
-    samp = old[[1]]
-    regrid=layer.apply(In,FUN.memSafe,FUN,samp,...)
+    In  = brick(fileTS3.1)[[1:12]]
+    old = brick(fileDetr)[[1:12]]
+    if (testRun) {
+        In  = In [[1:12]]
+        old = old[[1:12]]
+    }
+    
+    old    = layer.apply(old,FUN.memSafe,crop,y=extentDefault)
+    samp   = old[[1]]
+    regrid = layer.apply(In,FUN.memSafe,FUN,samp,...)
     return(list(old,regrid))
 }
